15f0b599759a0dd2fb26b4087f4c22591b100681
Croustys <akos.barabas2002@gmail.com>
2022-06-18 22:23:00 +0200

    proper http statusmsg response

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -13,23 +13,19 @@ type UserS struct {
 	Password string
 }
 
+func unAuthHttpResponse(w *http.ResponseWriter, msg string) {
+	json, err := json.Marshal(map[string]string{"StatusMessage": msg})
+	if err != nil {
+		log.Println(err)
+	}
+	(*w).WriteHeader(http.StatusUnauthorized)
+	(*w).Write(json)
+}
+
 func Login(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var u UserS
-	err := json.NewDecoder(r.Body).Decode(&u)
 
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	isLoginSuccessful := db.LoginUser(u.Email, u.Password)
-	if !isLoginSuccessful {
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-
-	if auth.AuthUser(r) {
+	if auth.AuthUser(r) { //return early if request has the correct jwt cookie
 		json, err := json.Marshal(map[string]string{"StatusMessage": "Login Successful"})
 		if err != nil {
 			log.Println(err)
@@ -37,6 +33,20 @@ func Login(w http.ResponseWriter, r *http.Request) {
 
 		w.WriteHeader(http.StatusOK)
 		w.Write(json)
+		return
+	}
+
+	var u UserS
+	err := json.NewDecoder(r.Body).Decode(&u)
+
+	if err != nil {
+		unAuthHttpResponse(&w, "Missing Credentials")
+		return
+	}
+
+	isLoginSuccessful := db.LoginUser(u.Email, u.Password)
+	if !isLoginSuccessful {
+		unAuthHttpResponse(&w, "Wrong Credentials")
 		return
 	}
 
@@ -81,4 +91,7 @@ func Register(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write(json)
+}
+func CreatePost(w http.ResponseWriter, r *http.Request) {
+	//create post
 }
