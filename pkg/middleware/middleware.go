ca5ba5c533069a4457072a72626ea47c69f54dbc
Croustys <akos.barabas2002@gmail.com>
2022-07-07 12:24:11 +0200

    added auth middleware

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -1,14 +1,39 @@
 package middleware
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/Croustys/blog-backend/internal/auth"
 )
+
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
+}
+
+var allowedSlugs []string = []string{"/login", "/register", "/ping"}
 
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Middleware")
-		//@TODO: handle & auth routes
-		next.ServeHTTP(w, r)
+		slug := r.URL
+		if contains(allowedSlugs, slug.Path) {
+			next.ServeHTTP(w, r)
+		} else if auth.AuthUser(r) {
+			next.ServeHTTP(w, r)
+		} else {
+			json, err := json.Marshal(map[string]string{"StatusMessage": "Unauthorized"})
+			if err != nil {
+				log.Println(err)
+			}
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write(json)
+		}
 	})
 }
